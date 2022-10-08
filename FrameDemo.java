0880eff3cd73977191158ca5a495d0a818b62645
Pratiksha Patil <113929757+PratikshaPatil18@users.noreply.github.com>
2022-10-08 21:51:34 +0530

    frame demo
    commit here

---
+++ FrameDemo.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+class FrameDemo extends Frame
+{
+public static void main(String a[])
+{
+FrameDemo f1=new FrameDemo();
+f1.setVisible(true);
+f1.setTitle("Pratiksha");
+f1.setSize(500,500);
+}
+ 
+}
